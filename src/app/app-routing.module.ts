b4a9cac50d164f4da4da46e349b00bf41cd6b278
Andres Hidalgo <andreshidalgo08@gmail.com>
2021-02-02 19:01:34 -0500

    Added initial components; Configured initial routing;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BodyComponent} from './body/body.component';
+import {BlankComponent} from './blank/blank.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'past-trials', component: BlankComponent},
+  {path: 'how-it-works', component: BlankComponent},
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
